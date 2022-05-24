62eed1b26c687628135700585e498ef2d3c3301d
Fishdestroyer <jeffwhitner@gmail.com>
2022-05-23 21:27:06 -0500

    Updated server typedefs and resolvers

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    reviews: Int
+
+}
+
+type Reviews {
+    _id: ID
+    reviewsText: String
+    createdAt: String
+    username: String
+    reactions: [Reaction]
+}
+
+type Reaction {
+_id: ID
+reactionBody: String
+createdAt: String
+username: String
+}
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    reviews(username: String!): [Reviews]
+    review(_id: ID!): Reviews
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addReviews(reviewsText: String!): Reviews
+    addReaction(reviewsId: ID!, reactionBody: String!): Reviews
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`;
+
+module.exports = typeDefs;
